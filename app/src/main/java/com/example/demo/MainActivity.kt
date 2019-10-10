da6d78317e6ccfcb4213c45271cf9b4ce00cd8ce
Daishin16 <sakuya1202@icloud.com>
2019-10-10 14:48:41 +0900

    Intent動かないよぉ

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -1,12 +1,58 @@
 package com.example.demo
 
+import android.content.Intent
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
-
+    var word = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            word = editText.text.toString()
+            val intent = Intent(this, ResultActivity::class.java)
+            MyAsyncTask().execute()
+            startActivity(intent)
+        }
     }
+
+    inner class MyAsyncTask : AsyncTask<Void, Void, String>() {
+
+        override fun doInBackground(vararg p0: Void?): String? {
+            return getHtml()
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            val imageUrlList = arrayListOf<String>()
+            val regex = "<img.+?src=\"(.+?)\".+?>"
+            val pattern = Pattern.compile(regex)
+            val matcher = pattern.matcher(result)
+            while (matcher.find()) {
+                imageUrlList.add(matcher.group(1))
+            }
+            intent.putExtra("list", imageUrlList)
+
+        }
+    }
+
+
+    fun getHtml(): String {
+        val client = OkHttpClient()
+        val req = Request.Builder().url("http://www.google.com/search?q=${word}&tbm=isch")
+            .header("UA", "UA:Mozilla/4.0 (compatible; MSIE 5.5; Windows 98)").get().build()
+        val resp = client.newCall(req).execute()
+        return resp.body!!.string()
+
+    }
+
 }
---
+++ app/src/main/java/com/example/demo/ResultActivity.kt
@@ -0,0 +1,20 @@
+package com.example.demo
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        val intent = getIntent()
+        val imageUrlList = intent.getIntegerArrayListExtra("list")
+        Glide.with(this)
+            .load(imageUrlList.get(0))
+            .into(imageView)
+    }
+}
