f1a5653b8090fb8bf98d6f07ba8c36bb47ff5c2e
Daishin16 <sakuya1202@icloud.com>
2019-10-31 11:35:23 +0900

    add recyclerview, fix indent

--- app/src/main/java/com/example/demo/RecyclerAdapter.kt
+++ app/src/main/java/com/example/demo/RecyclerAdapter.kt
@@ -4,21 +4,17 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.item.view.*
 
-class RecyclerAdapter(private val context: Context,  private val imageUrlList: List<String>) :
+class RecyclerAdapter(private val context: Context, private val imageUrlList: List<String>) :
     RecyclerView.Adapter<RecyclerAdapter.MyViewHolder>() {
 
-    class MyViewHolder(view:View) : RecyclerView.ViewHolder(view)
+    class MyViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): MyViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item,parent,false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
         return MyViewHolder(view)
     }
 
--- app/src/main/java/com/example/demo/ResultActivity.kt
+++ app/src/main/java/com/example/demo/ResultActivity.kt
@@ -16,30 +16,17 @@ class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
-        //val plain = findViewById<EditText>(R.id.editText2)
-        val intent = getIntent()
 
+        val intent = getIntent()
 
         val imageUrlList = intent.getStringArrayListExtra("list")
 
-        //plain.setText(intent.getStringExtra("word"))
-        //val imageView = findViewById<ImageView>(R.id.ImageView)
         val recycler = findViewById<RecyclerView>(R.id.recyclerview).apply {
             setHasFixedSize(true)
-            layoutManager = GridLayoutManager(baseContext,2)
-            adapter = RecyclerAdapter(context,imageUrlList)
+            layoutManager = GridLayoutManager(baseContext, 2)
+            adapter = RecyclerAdapter(context, imageUrlList)
 
         }
-
-
-        /*
-        Glide.with(this)
-            .load(imageUrlList.get(0))
-            .into(imageView)
-         */
-
-
-        
 
     }
 
